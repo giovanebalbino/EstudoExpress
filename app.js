4b3cc59d6f53bd06a50be4f319433713ece71847
Giovane <giovanebalbino@hotmail.com>
2021-04-19 22:27:25 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+//indica para express que usaremos os dados em JSON
+app.use(express.json());
+
+let usuarios = [
+    {nome: "Giovane", sobrenome: "Balbino"}
+];
+
+// http://localhost:3000/inicio
+app.get("/inicio", (request, response) => {
+    return response.send("to tentando pelo amor de Deus !");
+})
+
+// http://localhost:3000/usuarios
+app.get("/usuarios", (request, response) => {
+    return response.json(usuarios);
+})
+
+// http://localhost:3000/usuarios
+app.post("/usuarios", (request, response) => {
+    const { nome, sobrenome } = request.body;
+
+    usuarios.push({ nome, sobrenome });
+
+    return response.json({ nome, sobrenome });
+})
+
+// http://localhost:3000/usuarios/hendy
+app.put("/usuarios/:nome", (request, response) => {
+    const { nome } = request.params;
+
+    const { sobrenome } = request.body;
+
+
+
+})
+
+app.listen(3000, (req, res) => {
+    console.log("Servidor rodando")
+})
